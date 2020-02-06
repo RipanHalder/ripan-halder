734c40eb9a78e39d7630d86f3660a9a3c3ad35f8
Ripan Halder <halder.r@husky.neu.edu>
2020-02-06 03:52:28 -0500

    google analytics initial integration

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
+import ReactGA from 'react-ga';
+
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 
 // pages for this product
-// import Components from "views/Components/Components.js";
+import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
+import ProjectMedicalCorpsPage from "views/ProjectMedicalCorpsPage/ProjectMedicalCorpsPage.js";
+import LittleThings from "views/Blogs/LittleThings.js";
+import CycleOfLoveAndLoatheWithMangalore from "views/Blogs/CycleOfLoveAndLoatheWithMangalore.js";
+import ResumePage from "views/ResumePage/ResumePage.js";
 
+import IceBreaker from "views/Blogs/IceBreaker.js";
 var hist = createBrowserHistory();
 // var sslRedirect = require("heroku-ssl-redirect");
 // const app = express();
@@ -21,9 +28,20 @@ ReactDOM.render(
       <Route path="/landing-page" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/login-page" component={LoginPage} />
-      {/* <Route path="/" component={Components} /> */}
+      <Route path="/comp" component={Components} />
+      <Route path="/mcms" component={ProjectMedicalCorpsPage} />
+      <Route path="/little-things" component= {LittleThings} />
+      <Route path="/cycle-of-love-and-loathe-with-mangalore" component = {CycleOfLoveAndLoatheWithMangalore} />
+      <Route path="/ice-breaker" component= {IceBreaker} />
+      <Route path="/aboutme" component= {ResumePage} />
       <Route path="/" component={ProfilePage} />
     </Switch>
   </Router>,
   document.getElementById("root")
 );
+
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-123791717-1');
+  ReactGA.pageview('/');
+}
